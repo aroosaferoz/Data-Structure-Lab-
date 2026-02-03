37e71fa019c5213dfadcee7f8e93a0ebf4972eff
Aroosa Feroz <aroosaferoz1@gmail.com>
2026-02-03 10:54:58 +0500

    Add files via upload
    This lab focuses on basic operations on arrays. In this lab, three tasks were performed to understand how arrays work and how data can be accessed and modified using indexes.
    Tasks Included:
    1.Array Searching - Program to search a given number in an array and display its position if found.
    2.Update Value at Index - Program to update the value of an array element at a specific index.
    3.Array Reverse - Program to reverse the elements of an array.

---
+++ Lab 1 Task 2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+	int main(){
+	int arr[5] = {100, 200, 400, 800, 1600};
+	
+	int index = 1;
+	int newValue = 175;
+		
+	arr[index] = newValue;
+		for(int i = 0; i < 5; i++) {
+			cout << arr[i] << " ";
+}
+	return 0;	
+	
+	}
